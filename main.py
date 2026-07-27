08741d6ab3eadc39d9b4863bb230bfd9514db3ff
Hernan Araya <hl586@hotmail.com>
2026-07-26 20:19:56 -0600

    Add the scrolling_load_content function

--- main.py
+++ main.py
@@ -5,9 +5,34 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-# Create Selenium Chrome browser instance
+# Function for creating Selenium Chrome browser instance
 def create_driver():
     options = Options() # creates a configuration object for chrome
     options.add_experimental_option("detach", True) # Keep the browser window open after script finishes
     driver = webdriver.Chrome(options=options) # Starts a new Chrome browser with options configutation
     return driver
+
+# Function for Scroll and extract chapters and lessons
+def scrolling_load_content(driver, pause=1.2, max_times = 30): 
+    """
+    This function scroll the browser for content and wait a bit and try a few times if needed.
+
+    Args:
+        driver (object): It represents the browser session created by Selenium.
+        pause (float): It is the amount of time the function waits between each scroll step.
+        max_times(int): It is the maximum number of times the loop will run.
+    """
+
+    scroll_height = driver.execute_script("return document.body.scrollHeight") # It is about to know how tall is whole page.
+    times = 0
+    while times < max_times:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Run JS inside the browser and go down to the full height of the page
+        time.sleep(pause)
+        new_height = driver.execute_script("return document.body.scrollHeight") # Measure the page height again after the scroll
+        if new_height == scroll_height:
+            times += 1
+        else:
+            times = 0
+            scroll_height = new_height
+
+
